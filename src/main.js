3660c0c9651d806d81e9dd6511b76383238ae2ed
xiaomeng9 <951330162@qq.com>
2022-07-01 19:56:05 +0800

    gengxin

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 import Vue from 'vue'
-import App from './components/移动端.vue'
+import App from './components/案例品牌管理.vue'
 
 Vue.config.productionTip = false
 
@@ -8,3 +8,15 @@ import 'bootstrap/dist/css/bootstrap.css'
 new Vue({
   render: h => h(App),
 }).$mount('#app')
+
+// //在全局 注册过滤器
+// // vue.filter('过滤器名称','对应的处理函数')
+// Vue.filter('toDown',(val)=>{
+//   return val.toLowerCase()
+// })
+// Vue.filter('toReverse',(val)=>{
+//   return val.split('').reverse().join('')
+// })
+// // new Vue({
+// //   render:(h)=>h(App)
+// // }).$mount("#app")
